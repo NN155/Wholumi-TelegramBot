20c2d77ffa4e4c7c4a0bdafdb98b793a754f9ec1
Stetsiuk Oleksandr <StetONN155@gmail.com>
2025-03-06 23:17:05 +0200

    remove body-parser

--- db-server/src/app.js
+++ db-server/src/app.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const cors = require('cors');
 const routes = require('./routes');
 const express = require('express');
-const bodyParser = require('body-parser');
 const connectDB = require('./config/mongoDB.js');
 
 connectDB();
@@ -13,7 +12,6 @@ const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
-app.use(bodyParser.json());
 
 app.use("/", routes);
 
