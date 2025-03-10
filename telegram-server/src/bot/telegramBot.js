9cce6e69349b5a2abc043c83b90199aa5d5dc898
Stetsiuk Oleksandr <StetONN155@gmail.com>
2025-03-10 22:18:34 +0200

    rework bot actions

--- telegram-server/src/bot/telegramBot.js
+++ telegram-server/src/bot/telegramBot.js
@@ -4,8 +4,9 @@ const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, {
     polling: true
 });
 
+botService.bot = bot;
 
-bot.onText(/\/(trade|need|users) (\d+) (.+)/, async (msg, match) => {
+bot.onText(/^\/(trade|need|users) (\d+) (\S+)$/, async (msg, match) => {
     try {
         const action = {
             action: match[1],
@@ -23,6 +24,114 @@ bot.onText(/\/(trade|need|users) (\d+) (.+)/, async (msg, match) => {
         console.error('Error on /trade|need|users:', error);
     }
 
+});
+
+bot.onText(/^\/ban (\d+)([mhdM])$/, async (msg, match) => {
+    try {
+        if (!msg.reply_to_message) return;
+        
+        const targetUser = msg.reply_to_message.from;
+        
+        if (targetUser.is_bot) return;
+        
+        const action = {
+            userId: targetUser.id,
+            username: targetUser.username ? '@' + targetUser.username : null,
+            timeValue: match[1],
+            timeUnit: match[2],
+            banType: 'temporary',
+        }
+        
+        const msgData = {
+            chatId: msg.chat.id,
+            threadId: msg.message_thread_id || null,
+            messageId: msg.message_id,
+            from: msg.from,
+        }
+        
+        await botService.ban(action, msgData);
+    } catch (error) {
+        console.log('Error on /ban with time:', error);
+    }
+});
+
+bot.onText(/^\/ban$/, async (msg) => {
+    try {
+        if (!msg.reply_to_message) return;
+        if (msg.reply_to_message.message_id === msg.message_thread_id) return;
+
+        const targetUser = msg.reply_to_message.from;
+        
+        if (targetUser.is_bot) return;
+        
+        const action = {
+            userId: targetUser.id,
+            banType: 'permanent',
+        }
+        
+        const msgData = {
+            chatId: msg.chat.id,
+            threadId: msg.message_thread_id || null,
+            messageId: msg.message_id,
+            from: msg.from,
+        }
+
+        await botService.ban(action, msgData);
+    } catch (error) {
+        console.log('Error on /ban permanent:', error);
+    }
+});
+
+bot.onText(/^\/unban$/, async (msg) => {
+    try {
+        if (!msg.reply_to_message) return;
+        if (msg.reply_to_message.message_id === msg.message_thread_id) return;
+
+        const targetUser = msg.reply_to_message.from;
+
+        if (targetUser.is_bot) return;
+        
+        const action = {
+            userId: targetUser.id,
+        }
+        
+        const msgData = {
+            chatId: msg.chat.id,
+            threadId: msg.message_thread_id || null,
+            messageId: msg.message_id,
+            from: msg.from,
+            targetMessageId: msg.reply_to_message.message_id
+        }
+        
+        await botService.unban(action, msgData);
+    } catch (error) {
+        console.log('Error on /unban:', error);
+    }
+});
+
+bot.onText(/^\/status$/, async (msg, match) => {
+    try {
+        if (!msg.reply_to_message) return;
+        if (msg.reply_to_message.message_id === msg.message_thread_id) return;
+        
+        const targetUser = msg.reply_to_message.from;
+
+        if (targetUser.is_bot) return;
+        
+        const action = {
+            userId: targetUser.id,
+        }
+
+        const msgData = {
+            chatId: msg.chat.id,
+            threadId: msg.message_thread_id || null,
+            messageId: msg.message_id,
+            from: msg.from,
+        }
+        await botService.status(action, msgData);
+    } catch (error) {
+        console.log('Error on /unban:', error);
+    }
 });
 
 bot.on('polling_error', (error) => {
--- telegram-server/src/services/bot/index.js
+++ telegram-server/src/services/bot/index.js
@@ -1,8 +1,109 @@
 class BotService {
-    async searchCards(action, msgData) {
-        if (await this._chatAccess(msgData.chatId, msgData.threadId)) {
-            await this.sendAction(action, msgData);
+    constructor() {
+        this.bot = null;
+    }
+    async searchCards(action, { chatId, threadId, messageId, from }) {
+        if (!await this._chatAccess(chatId, threadId)) return;
+        const { access, message } = await this._getUserAccess(from.id, "searchCards");
+        if (!access) {
+            await this._sendBackMessage({ chatId, threadId, messageId }, message);
+            return;
         }
+        await this._sendAction("searchCards", action, { chatId, threadId, messageId, from });
+    }
+
+    async ban({ banType, timeValue, timeUnit, username, userId }, { chatId, threadId, messageId, from }) {
+        if (!await this._chatAccess(chatId, threadId)) return;
+        const permission = banType === 'permanent' ? 'permanentBan' : 'temporaryBan';
+        let { access, message } = await this._getUserAccess(from.id, permission);
+        if (!access) {
+            await this._sendBackMessage({ chatId, threadId, messageId }, message);
+            return;
+        }
+
+        let banDuration = null;
+        if (banType === 'temporary') {
+            const timeUnits = {
+                m: 1,
+                h: 60,
+                d: 60 * 24,
+                M: 60 * 24 * 30,
+            }
+            banDuration = timeValue * timeUnits[timeUnit];
+        }
+        const result = await this._ban({ telegramId: userId, telegramUsername: username, banDuration, banType });
+        await this._sendBackMessage({ chatId, threadId, messageId }, result.message);
+    }
+
+    async _ban({ telegramId, telegramUsername, banDuration, banType }) {
+        const response = await fetch(`${process.env.DB_SERVER_URL}/action/ban`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                telegramId,
+                telegramUsername,
+                banDuration,
+                banType
+            })
+        });
+        return await response.json();
+    }
+
+    async unban({ userId }, { chatId, threadId, messageId, from }) {
+        if (!await this._chatAccess(chatId, threadId)) return;
+        const { access, message } = await this._getUserAccess(from.id, "temperoryUnban");
+        if (!access) {
+            await this._sendBackMessage({ chatId, threadId, messageId }, message);
+            return;
+        }
+        const response = await this._getUserAccess(from.id, "permanentUnban");
+
+        const result = await this._unban({ telegramId: userId, permanent: response.access });
+        await this._sendBackMessage({ chatId, threadId, messageId }, result.message);
+    }
+
+    async _unban({ telegramId, permanent }) {
+        const response = await fetch(`${process.env.DB_SERVER_URL}/action/unban`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                telegramId,
+                permanent,
+            })
+        });
+        return await response.json();
+    }
+
+    async status({ userId }, { chatId, threadId, messageId, from }) {
+        if (!await this._chatAccess(chatId, threadId)) return;
+        const { access, message } = await this._getUserAccess(from.id, "status");
+
+        if (!access) {
+            await this._sendBackMessage({ chatId, threadId, messageId }, message);
+            return;
+        }
+
+        const result = await this._status({ telegramId: userId });
+
+        await this._sendBackMessage({ chatId, threadId, messageId }, result.message);
+    }
+
+    async _status({ telegramId }) {
+        const url = `${process.env.DB_SERVER_URL}/action/status`;
+        let response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                telegramId,
+            })
+        });
+        return await response.json();
     }
 
     async _chatAccess(chatId, threadId = null) {
@@ -12,9 +113,9 @@ class BotService {
         return !!data;
     }
 
-    async sendAction(action, messageData) {
+    async _sendAction(mode, action, messageData) {
         console.log("Telegram-server: send action", action);
-        const response = await fetch(`${process.env.MAIN_SERVER_URL}/telegram/searchCards`, {
+        const response = await fetch(`${process.env.MAIN_SERVER_URL}/telegram/${mode}`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -25,6 +126,39 @@ class BotService {
             })
         });
         return response;
+    }
+    async _getUserAccess(telegramId, action) {
+        let url = `${process.env.DB_SERVER_URL}/action/userAccess`;
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                telegramId,
+                action
+            })
+        });
+        const json = await response.json();
+        return json;
+    }
+
+    async _getUserIdByUsername(username, chatId) {
+        
+        try {
+            const member = await this.bot.getChatMember(chatId, username.replace(/^@/, ''));
+            return member?.user?.id ?? null;
+        } catch (error) {
+            console.error("Помилка отримання user ID:", error);
+            return null;
+        }
+    }
+
+    async _sendBackMessage({ chatId, threadId = null, messageId }, text) {
+        await this.bot.sendMessage(chatId, text, {
+            reply_to_message_id: messageId,
+            message_thread_id: threadId,
+        });
     }
 }
 module.exports = new BotService();
